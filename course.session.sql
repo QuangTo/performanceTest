8d1f00bc530f756b0e90ca785250ac1a6279bfcc
QuangTo <tvquangcs@gmail.com>
2023-08-05 09:08:40 +0700

    added mysql and data into table

---
+++ course.session.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE IF NOT EXISTS course_go(
+--     id INT   NOT NULL AUTO_INCREMENT,
+--     title VARCHAR(255),
+--     PRIMARY KEY(id)
+-- )
+
+-- SELECT * FROM course_go
+
+INSERT INTO course_go(title) VALUES("Leadership");
+
+-- DROP TABLE course_go;
--- src/course.go
+++ src/course.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	connectToDB()
+}
 
+func startServer() {
 	r := mux.NewRouter()
 	r.HandleFunc("/course-list/{id}", getCourseIDHandler).
 		Methods("GET")
@@ -19,4 +24,20 @@ func getCourseIDHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fmt.Fprintf(w, "course id : %s", id)
+}
+
+func connectToDB() {
+
+	db, err := sql.Open("mysql", "root:12341234@(127.0.0.1:3306)/mysql?parseTime=true")
+	// Initialize the first connection to the database, to see if everything works correctly.
+	// Make sure to check the error.
+	err := db.Ping()
+
+	// select the query
+	query := `Select * from course_go;`
+
+	// Executes the SQL query in our database. Check err to ensure there was no error.
+	_, err := db.Exec(query)
+
+	// fmt.Println(err)
 }
